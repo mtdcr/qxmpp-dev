f32b7a4dd05382a69a54fb184b9683389c0b7c0c
Andreas Oberritter <obi@saftware.de>
2011-09-23 18:32:22 +0200

    XEP-0016: Privacy Lists
    https://code.google.com/p/qxmpp/issues/detail?id=124
    
    Signed-off-by: Andreas Oberritter <obi@saftware.de>

--- src/base/QXmppConstants.h
+++ src/base/QXmppConstants.h
@@ -36,6 +36,8 @@ extern const char* ns_session;
 extern const char* ns_stanza;
 // XEP-0009: Jabber-RPC
 extern const char* ns_rpc;
+// XEP-0016: Privacy Lists
+extern const char *ns_privacy;
 // XEP-0020: Feature Negotiation
 extern const char* ns_feature_negotiation;
 // XEP-0030: Service Discovery
